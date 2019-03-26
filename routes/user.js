b4524e02eed99225900f2dfddfc6e0acbe3749b1
xiaode <2279323443@qq.com>
2019-03-26 19:31:22 +0800

    微信用户信息揭秘

--- routes/user.js
+++ routes/user.js
@@ -1,47 +1,64 @@
-// const { apisVarify } = require('../utils/verify');
+const { appid, appSecret, grantType } = require("../utils/params");
+const axios = require("axios");
+const WXBizDataCrypt = require("../utils/WXBizDataCrypt");
 module.exports = (app, con) => {
-    app.get('/user', (req, res) => {
-        var sql = `select * from wx_user where openid = '${req.query.openid}'`;
-        con.query(sql, (err, result) => {
-            if (err) {
-                res.status(500)
-                res.json({
-                    code: 500,
-                    msg: "获取失败",
-                    data: err
-                })
-            } else {
-                res.status(200)
-                res.json({
-                    code: 0,
-                    msg: "获取成功",
-                    data: result
-                })
-            }
+  app.get("/apis/user", (req, res) => {
+    var sql = `select * from wx_user where openid = '${req.query.openid}'`;
+    con.query(sql, (err, result) => {
+      if (err) {
+        res.status(500);
+        res.json({
+          code: 500,
+          msg: "获取失败",
+          data: err
         });
-    })
-    app.post('/user', (req, res) => {
-        var sql = `insert into wx_user(openid, nickname, avatarurl, gender, country, province, city, language, unionid, ctime) values ('${req.query.openid}','${req.query.nickname}','${req.query.avatarurl}',${req.query.gender},'${req.query.country}','${req.query.province}','${req.query.city}','${req.query.language}','${req.query.unionid}',${req.query.ctime})`;
-        con.query(sql, (err, result) => {
-            if (err) {
-                res.status(500)
-                res.json({
-                    code: 500,
-                    msg: "新建失败",
-                    data: err
-                })
-            } else {
-                res.status(200)
-                res.json({
-                    code: 0,
-                    msg: "新建成功",
-                    data: result
-                })
-            }
+      } else {
+        res.status(200);
+        res.json({
+          code: 0,
+          msg: "获取成功",
+          data: result
         });
-    })
-    app.put('/user', (req, res) => {
-        var sql = `update wx_user 
+      }
+    });
+  });
+  app.post("/apis/user", (req, res) => {
+    var url =
+      "https://api.weixin.qq.com/sns/jscode2session?appid=" +
+      appid +
+      "&secret=" +
+      appSecret +
+      "&js_code=" +
+      req.body.code +
+      "&grant_type=" +
+      grantType;
+    axios.get(url).then(response => {
+      res.json(new WXBizDataCrypt(appid, response.data.session_key).decryptData(
+        req.body.encryptedData,
+        req.body.iv
+      ));
+    });
+    // var sql = `insert into wx_user(openid, nickname, avatarurl, gender, country, province, city, language, unionid, ctime) values ('${req.query.openid}','${req.query.nickname}','${req.query.avatarurl}',${req.query.gender},'${req.query.country}','${req.query.province}','${req.query.city}','${req.query.language}','${req.query.unionid}',${req.query.ctime})`;
+    // con.query(sql, (err, result) => {
+    //     if (err) {
+    //         res.status(500)
+    //         res.json({
+    //             code: 500,
+    //             msg: "新建失败",
+    //             data: err
+    //         })
+    //     } else {
+    //         res.status(200)
+    //         res.json({
+    //             code: 0,
+    //             msg: "新建成功",
+    //             data: result
+    //         })
+    //     }
+    // });
+  });
+  app.put("/apis/user", (req, res) => {
+    var sql = `update wx_user 
         set 
             openid='${req.query.openid}',
             nickname='${req.query.nickname}',
@@ -54,43 +71,43 @@ module.exports = (app, con) => {
             unionid='${req.query.unionid}',
             ctime=${req.query.ctime} 
         where
-            openid = '${req.query.openid}'`
-        con.query(sql, (err, result) => {
-            if (err) {
-                res.status(500)
-                res.json({
-                    code: 500,
-                    msg: "更新失败",
-                    data: err
-                })
-            } else {
-                res.status(200)
-                res.json({
-                    code: 0,
-                    msg: "更新成功",
-                    data: result
-                })
-            }
+            openid = '${req.query.openid}'`;
+    con.query(sql, (err, result) => {
+      if (err) {
+        res.status(500);
+        res.json({
+          code: 500,
+          msg: "更新失败",
+          data: err
         });
-    })
-    app.delete('/user', (req, res) => {
-        var sql = `delete from wx_user where openid = '${req.query.openid}'`;
-        con.query(sql, (err, result) => {
-            if (err) {
-                res.status(500)
-                res.json({
-                    code: 500,
-                    msg: "删除失败",
-                    data: err
-                })
-            } else {
-                res.status(200)
-                res.json({
-                    code: 0,
-                    msg: "删除成功",
-                    data: result
-                })
-            }
+      } else {
+        res.status(200);
+        res.json({
+          code: 0,
+          msg: "更新成功",
+          data: result
         });
-    })
-}
+      }
+    });
+  });
+  app.delete("/apis/user", (req, res) => {
+    var sql = `delete from wx_user where openid = '${req.query.openid}'`;
+    con.query(sql, (err, result) => {
+      if (err) {
+        res.status(500);
+        res.json({
+          code: 500,
+          msg: "删除失败",
+          data: err
+        });
+      } else {
+        res.status(200);
+        res.json({
+          code: 0,
+          msg: "删除成功",
+          data: result
+        });
+      }
+    });
+  });
+};
---
+++ utils/params.js
@@ -0,0 +1,8 @@
+const appid = "wx8dcc3b5842c2ac80";
+const appSecret = "ccd00783023aeecbe79df42575d8052b";
+const grantType = "authorization_code";
+module.exports = {
+    appid,
+    appSecret,
+    grantType
+}
